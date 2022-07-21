9a38000a8f98b55d649bf78e2631c0b68ea407b7
André Almeida <andrefelipi2@hotmail.com>
2022-07-20 22:53:23 -0300

    aula 2

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { THEME } from "./src/styles/theme";
 
-import { SignIn } from './src/screens/SignIn';
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
     backgroundColor="transparent"
     translucent
     />
-      { fontsLoaded ? <SignIn /> : <Loading/>}
+      { fontsLoaded ? <Routes /> : <Loading/>}
   </NativeBaseProvider>
   );
 }
